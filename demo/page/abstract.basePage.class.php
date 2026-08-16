6ac3e335056ae734c9ee2df1649dfe6df2bb4887
alexCRAZY74 <alexei.baiandin@yandex.kz>
2026-08-17 01:13:39 +0500

    Update abstract.basePage.class.php

--- demo/page/abstract.basePage.class.php
+++ demo/page/abstract.basePage.class.php
@@ -1,24 +1,47 @@
 <?php
+declare(strict_types=1);
+
 namespace page;
-use \console;
+
+use console;
 
 abstract class basePage extends \TwigPage {
-  function __construct(){
+
+	public function __construct() {
 		$debug = true;
 		console::groupFunc();
-    parent::__construct();
+		parent::__construct();
+
 		$this->languageCode = \lang::current();
-		$this->actionURL = \App::urlServer().'/index.php';
-    $parts = explode('\\',  get_class($this));
-    array_shift($parts);
-    $this->PageID = implode("_", $parts);
-    $scriptName = "/demo/js/page.".implode(".", $parts).".js";
-    //\debug::outecho('test',\lang::getSection('pages'));
-    if ($debug) console::log('$scriptName',$scriptName);
-    if (file_exists(__DIR_ROOT_.$scriptName)) {
-      $this->pageJS = $scriptName;
-    }
-		if ($debug) console::log('$this', $this);
+		$this->actionURL = \App::urlServer() . '/index.php';
+
+		$parts = explode('\\', static::class);
+		array_shift($parts);
+
+		$this->PageID = implode('_', $parts);
+		$scriptName = '/demo/js/page.' . implode('.', $parts) . '.js';
+
+		// \debug::outecho('test', \lang::getSection('pages'));
+		if ($debug) {
+			console::log('$scriptName', $scriptName);
+		}
+
+		if (file_exists(__DIR_ROOT_ . $scriptName)) {
+			$this->pageJS = $scriptName;
+		}
+
+		if ($debug) {
+			console::log('$this', $this);
+		}
+
 		console::groupEnd();
-  }
+	}
+
+	public static function twig__pageURL(mixed ...$args): string {
+		return \App::urlToPage(...$args);
+	}
+
+	public static function twig__strcut(mixed ...$args): ?string {
+		return \App::strcut(...$args);
+	}
 }
