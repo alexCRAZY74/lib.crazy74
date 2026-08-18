8982281630862ecc9cd30ec4cfa6fc3fdffe5378
alexCRAZY74 <alexei.baiandin@yandex.kz>
2026-08-19 02:26:47 +0500

    вроде как релиз

---
+++ demo/page/class.ai_integration.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+namespace page;
+class ai_integration extends basePage {
+}
---
+++ demo/page/class.ui_templates.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+namespace page;
+class ui_templates extends basePage {
+}
